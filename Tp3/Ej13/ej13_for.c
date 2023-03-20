6c55b1db5a1dd1d2d91b75a74ac81ae49d5d7ae1
Za0s <za0s@localhost.localdomain>
2023-03-20 15:38:02 -0300

    Sexto commit: Agregue el ej13 Tp3

--- Tp3/Ej13/ej13_for.c
+++ Tp3/Ej13/ej13_for.c
@@ -3,9 +3,25 @@
 
 int main(){
 
-  int 
+  int p=0, i, a = getint("Decime el tamano del cuadrado:"); 
 
-  for 
+  for ( i=1; i<=a; i++ ){
+    
+    printf("*");
+    
+    if ( i==a ){
 
+      puts(" ");
+      i = 0;
+      p++;
+    }
+
+    if ( p == a ){
+
+      break;
+
+    }
+
+  }
 
 }

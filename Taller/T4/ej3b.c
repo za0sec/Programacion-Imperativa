3f15b1d528c325f0b5c547c640c71a954a472fee
Za0s <za0s@localhost.localdomain>
2023-03-31 20:01:31 -0300

    Commit: Corrijo Taller ej3b

--- Taller/T4/ej3b.c
+++ Taller/T4/ej3b.c
@@ -4,13 +4,17 @@ int intersection(const int v1[], const int v2[], int v3[], unsigned int dim1, un
 
   int j = 0, k = 0;
 
-  for ( int i=0; i<dim1 && k<dim2; i++ ){
+  for ( int i=0; (i<dim1 && k<dim2); i++ ){
 
     if ( v1[i] == v2[k] ){
     
         v3[j] = v1[i];
         j++;
         k++;
+    }else if( v2[k] < v1[i] ){
+
+      k++;
+      i--;
     }
 
   }
@@ -20,10 +24,10 @@ int intersection(const int v1[], const int v2[], int v3[], unsigned int dim1, un
 
 int main(){
 
-  int v1[] = {1,2,3,4,5,6,7,45,70,80};
-  int dim1 = 10;
-  int v2[] = {2,3,5,7,8,9,45};
-  int dim2 = 7;
+  int v1[] = {2,3,5,7,8,9,45,50,54,56,57,58,59,70,71,73,75,80};
+  int v2[] = {1,2,3,4,5,6,7,45,70,80};
+  int dim1 = sizeof(v1)/sizeof(v1[0]);
+  int dim2 = sizeof(v2)/sizeof(v2[0]);
 
   int v3[dim1];
 

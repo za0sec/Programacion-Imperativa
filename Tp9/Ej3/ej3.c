005c13075369d19cb2fac5177caea8b19a368986
za0s <za0s@localhost.localdomain>
2023-05-22 16:56:24 -0300

    Tp9 ej3

--- Tp9/Ej3/ej3.c
+++ Tp9/Ej3/ej3.c
@@ -2,21 +2,27 @@
  *representando dos vectores de números reales de igual tamaño y su dimensión. 
   La función debe regresar en su nombre el producto escalar de los vectores recibidos.
 */
+#include <stdio.h>
 
+int productoEsc(int * vector1, int * vector2, int dim);
 
 int main(){
 
-  int vector1[5] = {1,2,3,4,5};
-  int vector2[5] = {1,2,3,4,5};
-  int dimension = 5;
+  int vector1[5] = {1,2};
+  int vector2[5] = {1,2};
+  int dimension = 2;
 
+  printf("%d", productoEsc(vector1, vector2, dimension));
 
-
+  return 0;
 }
 
-int productoEsc(int vector1[], int vector2[], int dimension){
+int productoEsc(int vector1[], int vector2[], int dim){
 
-  
+ if (dim == 0)
+    return 0;
+
+  return ((*vector1) * (*vector2)) + productoEsc(vector1+1, vector2+1, dim-1);
 
 
 }

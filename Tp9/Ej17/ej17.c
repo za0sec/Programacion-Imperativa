0756753c7536b56e13e4ec77ec9f272a4dc16b4e
za0s <za0s@localhost.localdomain>
2023-05-28 19:11:53 -0300

    Tp9 ej17 Correccion

--- Tp9/Ej17/ej17.c
+++ Tp9/Ej17/ej17.c
@@ -26,12 +26,13 @@ int main(void) {
   char * not_zero[] ={ "a", "1", "a2", "abc2", "abc4", "abc33", "0123", "2aa", "$1", "@@2", "abcd22", "@@"};
 
   for(int i=0; i < sizeof(zero)/sizeof(zero[0]);i++) {
-    //printf("%s\n", zero[i]);
+    printf("%s\n", zero[i]);
     assert(bienformado(zero[i]) == 0);
+
   }    
 
   for(int i=0; i < sizeof(not_zero)/sizeof(not_zero[0]);i++) {
-    //printf("%s\n", not_zero[i]);
+    printf("%s\n", not_zero[i]);
     assert(bienformado(not_zero[i]) !=0);
  }
 
@@ -43,17 +44,12 @@ int bienformado(char * s){
 
   if (*s == '\0')
     return 0;
-  
-  int cont = 0;
-  while (isalpha(*s)){
-    cont++;
-    s++;
-  } 
 
-  if (isdigit(*s) && (*s - '0') == cont ){
-    return bienformado(s+1);
-  }
+  int i = bienformado(s+1); 
 
-  return 1;
+  if (i == 0 && isdigit(*s))
+    return (*s - '0');
+
+  return isalpha(*s) ? i-1 : -1;
 
 }

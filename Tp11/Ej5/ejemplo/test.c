bc4a9d32162b4c4a2370ab399b7a1026650cf185
za0s <za0s@localhost.localdomain>
2023-06-02 16:45:22 -0300

    TP11.

---
+++ Tp11/Ej5/ejemplo/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+  for (int i=0; i<10; i++){
+
+    putchar(i);
+
+  }
+
+
+  return 0;
+}
+
---
+++ Tp11/Ej5/ejemplo/test.s
@@ -0,0 +1,33 @@
+	.file	"test.c"
+	.intel_syntax noprefix
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L2
+.L3:
+	mov	eax, DWORD PTR -4[rbp]
+	mov	edi, eax
+	call	putchar@PLT
+	add	DWORD PTR -4[rbp], 1
+.L2:
+	cmp	DWORD PTR -4[rbp], 9
+	jle	.L3
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits

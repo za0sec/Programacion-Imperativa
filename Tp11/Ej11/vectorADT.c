054afb72397c833e2e39e361591e8ab2cdd75563
za0s <za0s@localhost.localdomain>
2023-06-08 18:47:47 -0300

    Resuelvo conflictos de merge

--- Tp11/Ej11/vectorADT.c
+++ Tp11/Ej11/vectorADT.c
@@ -3,10 +3,6 @@
 
 #define CHUNK 10
 
-<<<<<<< HEAD
-typedef struct vectorCDT{
-=======
->>>>>>> eb72f3e (Ej11 TERMINADO)
 
 typedef struct element{
     elemType elem;
@@ -32,77 +28,12 @@ vectorADT newVector(int (*compare)(elemType source, elemType target)){
 
 }
 
-<<<<<<< HEAD
-static void putRec(elemType *vec, size_t dimVec, elemType *elems, size_t dim, size_t index, size_t * count, int (*compare)(elemType source, elemType target) ) {
-=======
+
 static void putRec(element *vec, elemType *elems, size_t dim, size_t index, size_t * added, size_t * count){
->>>>>>> eb72f3e (Ej11 TERMINADO)
 
     if (!dim) {
         return;
     }
-<<<<<<< HEAD
-
-    if ( index == dimVec ) {
-        vec[index] = elems[(*count)];
-        (*count)++;
-        dimVec++;
-    }
-
-    putRec(vec, dimVec, elems, dim-1, index + 1, count, compare);
-}
-
-size_t put(vectorADT v, elemType *elems, size_t dim, size_t index) {
-
-  size_t count = 0;
-
-  if (index + dim > v->dim) {
-    v->vec = realloc(v->vec, index + dim + CHUNK);
-  }
-
-  putRec(v->vec, v->dim, elems, dim, index, &count, v->compare);
-
-  return count;
-
-}
-
-int getIdx(vectorADT v, elemType elem){
-
-  int flag = -1;
-
-  for(int i=0;  i<(v->dim) && flag == -1; i++){
-
-    if ( !(v->compare)((v->vec)[i], elem) ){
-      flag = i;
-    }
-
-  } 
-
-
-  return flag;
-
-}
-
-void freeVector(vectorADT v){
-
-  free(v->vec);
-    
-  free(v);
-
-}
-
-int elementCount(vectorADT v){
-  return v->dim; 
-}
-
-void deleteElement(vectorADT v, size_t index){
-
-  if (index < v->dim && index > 0){
-    for (size_t i=index; i<(v->dim)-1; i++){
-      (v->vec)[i] = (v->vec)[i+1];
-    }
-  }
-=======
 
     if (!(vec[index].used)) {
       vec[index].elem = elems[(*added)];
@@ -110,9 +41,8 @@ void deleteElement(vectorADT v, size_t index){
       (*added)++;
       (*count)++;
     }
-    
+  
   putRec(vec, elems, dim-1, index + 1, added, count);
->>>>>>> eb72f3e (Ej11 TERMINADO)
 
 }
 
@@ -166,9 +96,8 @@ int elementCount(vectorADT v){
 
 void deleteElement(vectorADT v, size_t index){
   if (index < v->dim && index >= 0 && v->vec[index].used){
-   
     v->vec[index].used = 0;
     (v->count)--;
-  
   }
 }
+

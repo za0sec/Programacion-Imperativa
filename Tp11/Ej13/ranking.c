d73501846f52b3ffb4a3950687c71bd67ca508a0
za0s <za0s@localhost.localdomain>
2023-06-09 18:38:37 -0300

    Ej12

---
+++ Tp11/Ej13/ranking.c
@@ -0,0 +1,12 @@
+
+typedef struct rankingCDT{
+
+  elemType * vec;
+  
+
+
+}rankingCDT;
+
+
+rankingADT newRanking(elemType elems[], size_t dim, int (*compare)(elemType source, elemType target));
+

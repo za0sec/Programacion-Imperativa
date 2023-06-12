dc82900c6607afc435b385d17e03d1a634908bbb
za0s <za0s@localhost.localdomain>
2023-06-12 19:43:23 -0300

    Ej15 inicio

--- Tp11/Ej15/moveToFrontADT.c
+++ Tp11/Ej15/moveToFrontADT.c
@@ -39,11 +39,7 @@ static int belongsRec(TList list, elemType elem){
         return 0;
     }
     
-    if (compare(list->elem, elem) < 0){
-        return belongsRec(list->tail, elem);
-    }
-    
-    return 1;
+    return belongsRec(list->tail, elem);
 } 
 
 
@@ -83,14 +79,69 @@ void toBegin(moveToFrontADT moveToFront){
 
 
 int hasNext(moveToFrontADT moveToFront){
-    return moveToFront->iterator->tail != NULL;
+    return moveToFront->iterator != NULL;
 }
 
 elemType next(moveToFrontADT moveToFront){
    if (hasNext(moveToFront->iterator)){
-
+        elemType element = moveToFront->iterator->elem;
+        moveToFront->iterator = moveToFront->iterator->tail;
+        return element;
     }
 
-    return NULL;
+    exit(1);
 }
 
+
+
+static TList belongsGet(TList list, elemType elem){
+
+    if (list == NULL){
+        return NULL;
+    }
+
+    if (!compare(list->elem, elem)){
+        TList aux = list->tail;
+        free(list);
+        return aux;
+    }
+    
+    list->tail = belongsGet(list->tail, elem);
+
+    return list;
+} 
+
+
+
+
+elemType * get(moveToFrontADT moveToFront, elemType elem){
+    
+
+    if (compare(moveToFront->last->elem, elem)){
+        TList aux = belongsGet(moveToFront->first, elem);
+        moveToFront->first->elem = elem;
+        moveToFront->first->tail = aux; 
+    }else{
+        TList aux = malloc(sizeof(TNode));
+        
+        aux->elem = 
+        moveToFront->first->elem = elem;
+        moveToFront->first->tail = moveToFront->first; 
+    }
+
+} 
+
+
+static TList getLast(TList list){
+
+    if (list == NULL || list->tail == NULL)
+        return NULL;
+
+    if(list->tail->tail == NULL){
+        last = list;
+        free(list->tail);
+    }
+
+    return getLast(list->tail);
+
+}

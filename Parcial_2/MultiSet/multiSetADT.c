5374e345e8ac2c2173d392a75db0f917b4ee89f7
za0s <za0s@localhost.localdomain>
2023-06-15 23:12:41 -0300

    Parciales2(Corregido)

--- Parcial_2/MultiSet/multiSetADT.c
+++ Parcial_2/MultiSet/multiSetADT.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "multiSetADT.h" 
 
 
@@ -58,21 +59,20 @@ static TList addRec(TList list, elemType elem, size_t * count, size_t * flag){
 
 }
 
+
 unsigned int add(multiSetADT multiSet, elemType elem){
 
     size_t count;
     size_t flag = 0;
 
     multiSet->first = addRec(multiSet->first, elem, &count, &flag);
+    multiSet->usedCount++;
     if (!flag){
-        multiSet->usedCount++;
-    }else{
         multiSet->count++;
-        multiSet->usedCount++;
     }
+
     return count;
 }
-
 
 static size_t countRec(TList list, elemType elem){
 
@@ -112,16 +112,25 @@ static TList removeOneRec(TList list, elemType elem, size_t * count, int * flag)
             TList aux = list->tail;
             free(list);
             list = aux;
-            (*flag)=2;
+            (*flag) = 2;
+        } else {
+            list->elem.used--;
+            if (list->elem.used == 0) {
+                TList aux = list->tail;
+                free(list);
+                list = aux;
+                (*flag) = 2;
+            } else {
+                (*flag) = 1;
+            }
         }
-
-        list->elem.used--;
-        if (*flag != 2)
-            (*flag) = 1;
-        
-        (*count) = list->elem.used;
+        if (list != NULL) {
+            (*count) = list->elem.used;
+        }
+    
         return list;
     }
+
     
     if (compare(list->elem.value, elem) < 0){
         list->tail = removeOneRec(list->tail, elem, count, flag);
@@ -139,35 +148,34 @@ int removeOne( multiSetADT multiSet, elemType elem ){
 
     multiSet->first = removeOneRec(multiSet->first, elem, &count, &flag);
 
-    if (flag == 1){
+    if (!flag){
         multiSet->usedCount--;
     }else if (flag == 2){
         multiSet->count--;
     }
+
     return (int)count;
 }
 
 
-static TList removeAllRec(TList list, elemType elem, int * flag){
+static TList removeAllRec(TList list, elemType elem, int * flag, int *removedCount){
 
     if (list == NULL){
         return NULL;
     }
 
     if (!compare(list->elem.value, elem)){
-        while(list->elem.used){
-            list->elem.used--;
-            (*flag) = 1;
-        }
-        list->elem.used--;
+        *removedCount = list->elem.used;
+        list->elem.used = 0;
+        (*flag) = 1;
         TList aux = list->tail;
         free(list);
         list = aux;
         return list;
     }
-
+    
     if (compare(list->elem.value, elem) < 0){
-        list->tail = removeAllRec(list->tail, elem, flag);
+        list->tail = removeAllRec(list->tail, elem, flag, removedCount);
     }
 
     return list;
@@ -178,11 +186,15 @@ static TList removeAllRec(TList list, elemType elem, int * flag){
 void removeAll( multiSetADT multiSet , elemType elem ){
     
     int flag = 0;
+    int removedCount = 0;
 
-    multiSet->first = removeAllRec(multiSet->first, elem, &flag);
+    multiSet->first = removeAllRec(multiSet->first, elem, &flag, &removedCount);
     
-    if (flag)
-        multiSet->usedCount++;
+     if (flag) {
+        multiSet->count--;
+        multiSet->usedCount -= removedCount;
+    }
+
 }
 
 
@@ -194,16 +206,13 @@ static elemType * minElemRec(TList list, int * dim, elemType * aux, int count){
     
     if (list->elem.used < count || count == -1){
         count = list->elem.used;
-        aux = realloc(aux, sizeof(elemType));
+        aux = malloc(sizeof(elemType));
         (*dim) = 1;
         aux[(*dim)-1] = list->elem.value;
-    }
-
-    if (list->elem.used == count){
+    }else if (list->elem.used == count){
         (*dim)++;
         aux = realloc(aux, (*dim)*sizeof(elemType));
         aux[(*dim)-1] = list->elem.value;
-        return minElemRec(list->tail, dim, aux, count);
     }
 
     return minElemRec(list->tail, dim, aux, count);
